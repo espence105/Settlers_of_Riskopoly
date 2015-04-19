c24455072128e50f615e54d60688b006679bc420
espence105 <ejspence@anderson.edu>
2015-04-19 15:40:01 -0400

    admin
    Admin page with jquery

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,15 @@
 /**
  * Created by Eric on 4/18/2015.
  */
-function getLastBuys(){
-
+function submitSell(){
+    $.post(
+           'http://posttestserver.com/post.php?dir=lookHERE',
+        {
+            resourceID: $("#resourceNumber").val(),
+            quantity:   $("#number").val(),
+            user:       $("#user").val()
+        },
+        function(data){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
 }
